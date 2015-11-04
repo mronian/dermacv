a3991a5feec76f1f3fcebd55f66237e00871b1cb
mronian <anchitn@gmail.com>
2015-11-04 22:17:32 +0530

    Asymmetry Features Calculated

--- dermacv/CVCode/Asymmetry.py
+++ dermacv/CVCode/Asymmetry.py
@@ -2,6 +2,20 @@ import numpy as np
 import cv2
 import math
 
+def calcB(img, x, y, angle):
+    
+    B1=B2=0
+    
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            val = j-angle*(i-x)-y
+            if val>0:
+                B1=B1+1
+            else :
+                B2=B2+1
+    
+    return B1, B2
+
 def calculateFeatures(filename):
     
     original=cv2.imread(filename, cv2.IMREAD_UNCHANGED)
@@ -20,6 +34,11 @@ def calculateFeatures(filename):
     perimeter = cv2.arcLength(cnt,True)
     (x,y),(L1,L2),angle = cv2.fitEllipse(cnt)
     
+    
+    B11, B12=calcB(original, x, y, angle)
+    B21, B22=calcB(original, x, y, 180-angle)
+    
+    
     f1_solidity=float(area)/hull_area
     f2_extent = float(area)/rect_area
     f3_equi_diameter = float(4*area)/(L1*np.pi)
@@ -29,8 +48,8 @@ def calculateFeatures(filename):
     f7_ratio_periarea=float(perimeter)/area
     f8_ratio_L1=float(B11-B12)/area
     f9_ratio_L2=float(B21-B22)/area
-    f10_ratio_areasL1=float(B11/B21)
-    f11_ratios_areasL2=float(B21/B22)
+    f10_ratio_areasL1=float(B11)/B21
+    f11_ratios_areasL2=float(B21)/B22
     
     features.append(f1_solidity)
     features.append(f2_extent)
@@ -43,10 +62,6 @@ def calculateFeatures(filename):
     features.append(f9_ratio_L2)
     features.append(f10_ratio_areasL1)
     features.append(f11_ratios_areasL2)
-    
-    print features
-    
-    M = cv2.moments(cnt)
     
     return features
 
